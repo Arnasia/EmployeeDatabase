559b48ab2766830563d3ac3e3c14e296694ca826
Arnasia Jackson <arnasial.jackson@gmail.com>
2023-03-15 22:22:59 -0400

    added selection got choices

--- general.js
+++ general.js
@@ -7,6 +7,42 @@ const beginPrompt = () => {
         type: 'list',
         name: 'initialPrompt',
         message: 'What action would you like to take?',
-        choices: ['View All Departments', 'Add Department', 'View Employees by Department','View All Employees', 'Add Employee', 'Update Employee Role', 'Delete Employee', 'View All Roles', 'Add Role', 'Delete Role', 'View Employees by Manager', 'Update Employee Manager'],
+        choices: ['View All Departments', 'Add Department','View All Employees', 'Add Employee', 'Update Employee Role', 'Delete Employee', 'View All Roles', 'Add Role', 'Delete Role'],
     }])
-}
+    .then(answer => {
+        if (answer.initialPrompt === 'View all departments') {
+          return viewDepartments();
+  
+        } else if (answer.initialPrompt === 'Add departments') {
+          return addDepartment();
+  
+        } else if (answer.initialPrompt === 'View all departments') {
+          return viewAllEmployees();
+  
+        } else if (answer.initialPrompt === 'Add an Employee') {
+          return addEmployee();
+  
+        } else if (answer.initialPrompt === 'Update an employee role') {
+          return updateEmployeeRole();
+  
+        } else if (answer.initialPrompt === 'Delete employee') {
+          return deleteEmployee();
+  
+        } else if (answer.initialPrompt === 'View all roles') {
+          return viewRoles();
+  
+        } else if (answer.initialPrompt === 'Add a role') {
+          return addRole();
+  
+        } else if (answer.initialPrompt === 'Delete a role') {
+          return deleteRole();
+
+        } else {
+          console.log('Please select an action!');
+          return false;
+        }
+      })
+  };
+
+
+
